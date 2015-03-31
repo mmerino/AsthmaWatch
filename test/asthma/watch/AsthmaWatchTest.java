9bf2bcf3ef6d6ba7e1bc58d430fda444555fc101
slckwd <s.lckwd@gmail.com>
2015-03-31 10:34:48 -0400

    Added objenesis, fixed tests

--- test/asthma/watch/AsthmaWatchTest.java
+++ test/asthma/watch/AsthmaWatchTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;
@@ -14,44 +13,67 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AsthmaWatchTest {
 
 	HttpServletRequest request = mock(HttpServletRequest.class);
 	HttpServletResponse response = mock(HttpServletResponse.class);
 	String zip = "48145";
 
-//	@Test
-//	public void tooSmallZipTriggersGuardClause() {
-//	}
-
 	@Test
-	public void emptyJsonReturnsEmptyString() throws Exception {
+	public void emptyJsonSetsNoAttributes() throws Exception {
 		URL url = getMockUrlContents("");
-		new AsthmaWatch(request, response, url, url);
+		AsthmaWatch aw = new AsthmaWatch();
+		aw.pollenInformation(request, response, url);
+		aw.weatherInformation(request, response, url);
 		verify(request, never()).setAttribute(anyString(), anyDouble());
 		verify(request, never()).setAttribute(anyString(), anyString());
 	}
 
-//	@Test
-//	public void populatedJsonReturnsJson() {
-//
-//	}
-//
-//	@Test
-//	public void emptyJsonSetsNoAttributes() {
-//
-//	}
-//
-//	@Test
-//	public void populatedJsonSetsAttributes() {
-//	}
-//
-//	@Test
-//	public void malformedJsonSetsNoAttributes() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	public void emptyJsonReturnsNullString() throws Exception {
+		URL url = getMockUrlContents("");
+		AsthmaWatch aw = new AsthmaWatch();
+		String actual = aw.getJson(url);
+		String expected = null;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void populatedJsonSetsAttributesForPollen() throws Exception {
+		String json = "\"{\\\"pollenForecast\\\":{\\\"forecast\\\":[1.0,2.0,3.0,4.0],\\\"pp\\\":\\\"Treeant.\\\"}}\"";
+		URL url = getMockUrlContents(json);
+		AsthmaWatch aw = new AsthmaWatch();
+		aw.pollenInformation(request, response, url);
+		verify(request).setAttribute("day1", 1.0);
+		verify(request).setAttribute("day2", 2.0);
+		verify(request).setAttribute("day3", 3.0);
+		verify(request).setAttribute("day4", 4.0);
+		verify(request).setAttribute("pp", "Treeant.");
+	}
+
+	@Test
+	public void populatedJsonSetsAttributesForWeather() throws Exception {
+		String json = "{\"current_observation\":{\"relative_humidity\": \"50%\"}}";
+		URL url = getMockUrlContents(json);
+		AsthmaWatch aw = new AsthmaWatch();
+		aw.weatherInformation(request, response, url);
+		verify(request).setAttribute("humidity", "50%");
+	}
+
+	@Test
+	public void populatedJsonReturnsJsonString() throws Exception {
+		String expected = "{\"current_observation\":{\"relative_humidity\": \"50%\"}}";
+		URL url = getMockUrlContents(expected);
+		AsthmaWatch aw = new AsthmaWatch();
+		String actual = aw.getJson(url);
+		assertEquals(expected, actual);
+	}
 
 	public URL getMockUrlContents(String contents) throws Exception {
 		URLConnection mockConnection = mock(URLConnection.class);
