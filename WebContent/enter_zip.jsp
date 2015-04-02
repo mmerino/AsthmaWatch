22da9cec86f3ad22f89ac7e43471d2fe5ff33cfe
mjowers <melissa.anne.jowers@gmail.com>
2015-04-02 11:35:07 -0400

    Message request working
    If user input is null, empty, or less than 5 characters a prompt and/or
    message tells users to input valid zip

--- WebContent/enter_zip.jsp
+++ WebContent/enter_zip.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
@@ -9,16 +9,13 @@
 <body>
 	<p>Please enter your zip code for the current asthma information
 		and then click the submit button.</p>
-	<c:if test="${message != null}">
 		<p>
 			<i>${message}</i>
 		</p>
-	</c:if>
 	<form action="asthmawatch" method="post">
-		<input type="hidden" name="action" value="add">
-		<label class="pad_top">Zip Code</label> <input type="text"
-		 	name="action"> <label>&nbsp;</label>
-		<input type="submit" value="Submit" class="margin_left">
+		<label class="pad_top">Zip Code</label> 
+		<input type="text" name="zip" placeholder = "enter zip code" required>
+		<input type="submit" value="Submit">
 	</form>
 </body>
 </html>
--- src/asthma/watch/Servlet.java
+++ src/asthma/watch/Servlet.java
@@ -1,6 +1,7 @@
 package asthma.watch;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,35 +17,46 @@ public class Servlet extends HttpServlet {
 		super();
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String zip = request.getParameter("zip");
+		if (zip == null || zip.isEmpty() || zip.length()<5) {
+			errorOuput(request, response);
+		}
+		else{
+		try {
+			AsthmaWatch aw = new AsthmaWatch(request, response, zip);
+			aw.setPollenInfo();
+			aw.setWeatherInfo();
+			// aw.setForecastInfo();
+			// aw.setAstronomyInfo();
+			aw.goToResults();
+		} catch (Exception e) {
+			request.setAttribute("message",
+					"There was an error: " + e.getMessage());
+			doGet(request, response);
+		}
+		}
+
+	}
+
+	// validate the parameters
+	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request,
+	private void errorOuput(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		String zip = request.getParameter("zip");
-		AsthmaWatch aw = new AsthmaWatch(request, response, zip);
-		aw.setPollenInfo();
-		aw.setWeatherInfo();
-//		aw.setForecastInfo();
-//		aw.setAstronomyInfo();
-		aw.goToResults();
+		String message = (String) request.getAttribute("message");
+		if (message == null) {
+			message = "Please fill in the zip code.";
+		}
+
+		request.setAttribute("message", message);
+		getServletContext().getRequestDispatcher("/enter_zip.jsp").forward(
+				request, response);
 	}
-	// validate the parameters
-//    String message;
-//    if (zipCode == null|| zipCode.isEmpty() {
-//        message = "Please enter a valid zip code.";
-//        url = "/enter_zip.jsp";
-//    } 
-//    else {
-//        message = null;
-//        url = "/thanks.jsp";
-//        UserDB.insert(user);
-//    }
-//    request.setAttribute("user", user);
-//    request.setAttribute("message", message);
-//    request.setAttribute("currentYear", currentYear);
-//    
-//}
 }
